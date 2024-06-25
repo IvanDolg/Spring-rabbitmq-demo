16abd97c4da848de2546e66b14da64ee25b6aca8
Ivan Dauhalaptseu <ivan.dauhalaptseu@gmail.com>
2024-06-25 11:13:24 +0300

    init: demo project with rabbitMq

---
+++ src/main/java/com/practice/springboot/config/RabbitMQConfig.java
@@ -0,0 +1,39 @@
+package com.practice.springboot.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+    @Value("${spring.rabbitmq.queue.name}")
+    private String queueName;
+
+    @Value("${spring.rabbitmq.exchange.name}")
+    private String topicExchange;
+
+    @Value("${spring.rabbitmq.routing.key}")
+    private String routingKey;
+
+    @Bean
+    public Queue queue() {
+        return new Queue(queueName);
+    }
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(topicExchange);
+    }
+
+    @Bean
+    public Binding binding() {
+        return BindingBuilder
+                .bind(queue())
+                .to(exchange())
+                .with(routingKey);
+    }
+}
